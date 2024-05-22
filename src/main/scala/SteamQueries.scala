f2d41a6422d05e98bfb0de4b50310e18dd97fdb9
lm-jim <luismigueljimenezaliaga1999@gmail.com>
2024-05-22 17:58:25 +0200

    Added Non-SQL version of the queries, using Spark Dataframe operations

--- src/main/scala/SteamQueries.scala
+++ src/main/scala/SteamQueries.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql._
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
@@ -53,66 +54,131 @@ object SteamQueries extends App {
 
   convertedDF.createOrReplaceTempView("steam_df")
 
+  //Valores para la generación de resultados:
+  val USE_SQL = true;        //Usar consultas directamente SQL
+  val USE_COL_EXP = true;    //Usar expresiones de columnas de Spark
+
   ////////////////////////////////
   ///////  MINICONSULTAS /////////
   ////////////////////////////////
 
   //Porcentaje de juegos con soporte de mando
-  val controllerSupportQuery = spark.sql(
-    s"""SELECT ControllerSupport, COUNT(*) * 100 / CAST(SUM(COUNT(*)) OVER () AS float)
+  if(USE_SQL) {
+    val controllerSupportQuery = spark.sql(
+      s"""SELECT ControllerSupport, COUNT(*) * 100 / CAST(SUM(COUNT(*)) OVER () AS float)
                 FROM steam_df
                 GROUP BY ControllerSupport""")
-  controllerSupportQuery.saveToCsv("GamesWithControllerSupport")
+
+    controllerSupportQuery.saveToCsv("SQL/GamesWithControllerSupport")
+  }
+  if(USE_COL_EXP) {
+    val controllerSuppCountDf = convertedDF.groupBy("ControllerSupport").count()
+    val controllerSupportQuery = controllerSuppCountDf
+      .withColumn("percentage", col("count") * 100 / convertedDF.count())
+      .select("ControllerSupport", "percentage")
+      .orderBy(desc("percentage"))
+
+    controllerSupportQuery.saveToCsv("NonSQL/GamesWithControllerSupport")
+  }
 
   //Porcentaje de juegos con algún tipo de DRM presente
-  val drmQuery = spark.sql(
-    s"""SELECT DRM, COUNT(*) * 100 / CAST(SUM(COUNT(*)) OVER () AS float) AS DRM_Percentage
+  if(USE_SQL) {
+    val drmQuery = spark.sql(
+      s"""SELECT DRM, COUNT(*) * 100 / CAST(SUM(COUNT(*)) OVER () AS float) AS DRM_Percentage
                 FROM steam_df
                 GROUP BY DRM
                 ORDER BY DRM_Percentage DESC""")
-  drmQuery.saveToCsv("GamesWithDRM")
+
+    drmQuery.saveToCsv("SQL/GamesWithDRM")
+  }
+  if(USE_COL_EXP) {
+    val drmCountDf = convertedDF.groupBy("DRM").count()
+    val drmQuery = drmCountDf
+      .withColumn("percentage", col("count") * 100 / convertedDF.count())
+      .select("DRM", "percentage")
+      .orderBy(desc("percentage"))
+
+    drmQuery.saveToCsv("NonSQL/GamesWithDRM")
+  }
 
   ////////////////////////////////////////
   ///////  CONSULTAS PRINCIPALES /////////
   ////////////////////////////////////////
 
+
   //Número de juegos lanzados en Steam por fecha
-  val gamesReleasedByDateQuery = spark.sql(
-    s"""SELECT ReleaseDate, COUNT(*) AS GamesReleased
-                FROM steam_df
-                GROUP BY ReleaseDate
-                ORDER BY ReleaseDate""")
-  gamesReleasedByDateQuery.saveToCsv("ReleaseDateCount")
+  if(USE_SQL){
+    val gamesReleasedByDateQuery = spark.sql(
+      s"""SELECT ReleaseDate, COUNT(*) AS GamesReleased
+                  FROM steam_df
+                  GROUP BY ReleaseDate
+                  ORDER BY ReleaseDate""")
+
+    gamesReleasedByDateQuery.saveToCsv("SQL/ReleaseDateCount")
+  }
+  if(USE_COL_EXP){
+    val gamesReleasedByDateQuery = convertedDF.groupBy("ReleaseDate").count().orderBy("ReleaseDate")
+
+    gamesReleasedByDateQuery.saveToCsv("NonSQL/ReleaseDateCount")
+  }
 
   //Distribución de precios de los juegos
-  val gamePriceDistributionQuery = spark.sql(
-    s"""SELECT CAST(REGEXP_REPLACE(REGEXP_REPLACE(Price, '[$$€]', ''), ',', '.') AS float) AS PriceFloat,
+  if(USE_SQL){
+    val gamePriceDistributionQuery = spark.sql(
+      s"""SELECT CAST(REGEXP_REPLACE(REGEXP_REPLACE(Price, '[$$€]', ''), ',', '.') AS float) AS PriceFloat,
                       COUNT(*) as GamesPrice
                 FROM steam_df
                 GROUP BY PriceFloat
                 ORDER BY PriceFloat""")
-  gamePriceDistributionQuery.saveToCsv("GamePriceDistribution")
+
+    gamePriceDistributionQuery.saveToCsv("SQL/GamePriceDistribution")
+  }
+  if(USE_COL_EXP){
+    val regexpPriceDf = convertedDF.withColumn("PriceFloat", regexp_replace(col("Price"), "[$€]", ""))
+      .withColumn("PriceFloat", regexp_replace(col("PriceFloat"), ",", ".").cast("float"))
+    val gamePriceDistributionQuery = regexpPriceDf.groupBy("PriceFloat").count().orderBy("PriceFloat")
+
+    gamePriceDistributionQuery.saveToCsv("NonSQL/GamePriceDistribution")
+  }
 
   //Número de cada tipo de producto en la tienda de Steam
-  val productTypeCountQuery = spark.sql(
-    s"""SELECT ProductType, COUNT(*) AS Count
+  if(USE_SQL){
+    val productTypeCountQuery = spark.sql(
+      s"""SELECT ProductType, COUNT(*) AS Count
                   FROM steam_df
                   GROUP BY ProductType
                   ORDER BY Count DESC""")
-  productTypeCountQuery.saveToCsv("ProductTypeCount")
+
+    productTypeCountQuery.saveToCsv("SQL/ProductTypeCount")
+  }
+  if(USE_COL_EXP){
+    val productTypeCountQuery = convertedDF.groupBy("ProductType").count().orderBy(desc("count"))
+
+    productTypeCountQuery.saveToCsv("NonSQL/ProductTypeCount")
+  }
 
   //Compatibilidad con sistemas operativos
-  val osCompatibilityQuery = spark.sql(
-    s"""SELECT WindowsCompatible, MacCompatible, LinuxCompatible,
+  if(USE_SQL){
+    val osCompatibilityQuery = spark.sql(
+      s"""SELECT WindowsCompatible, MacCompatible, LinuxCompatible,
                         COUNT(*) AS CompatibleGames
                   FROM steam_df
                   GROUP BY WindowsCompatible, MacCompatible, LinuxCompatible
                   ORDER BY CompatibleGames DESC""")
-  osCompatibilityQuery.saveToCsv("CompatibleGames")
+
+    osCompatibilityQuery.saveToCsv("SQL/CompatibleGames")
+  }
+  if(USE_COL_EXP){
+    val osCompatibilityQuery = convertedDF.groupBy("WindowsCompatible", "MacCompatible", "LinuxCompatible").count()
+      .orderBy(desc("count"))
+
+    osCompatibilityQuery.saveToCsv("NonSQL/CompatibleGames")
+  }
 
   //Categorías más comunes
-  val categoryQuery = spark.sql(
-    s"""SELECT Category, COUNT(*) AS GameCount
+  if(USE_SQL){
+    val categoryQuery = spark.sql(
+      s"""SELECT Category, COUNT(*) AS GameCount
                   FROM (
                         SELECT TRIM(Category) AS Category
                         FROM steam_df
@@ -120,11 +186,21 @@ object SteamQueries extends App {
                   )
                   GROUP BY Category
                   ORDER BY GameCount DESC""")
-  categoryQuery.saveToCsv("CategoryCount")
+
+    categoryQuery.saveToCsv("SQL/CategoryCount")
+  }
+  if(USE_COL_EXP){
+    val splittedCategoriesDf = convertedDF.withColumn("Category", explode(split(col("Categories"), ",")))
+    val categoryQuery = splittedCategoriesDf.withColumn("Category", trim(col("Category"))).groupBy("Category").count()
+      .orderBy(desc("count"))
+
+    categoryQuery.saveToCsv("NonSQL/CategoryCount")
+  }
 
   //Géneros más comunes
-  val genreQuery = spark.sql(
-    s"""SELECT Genre, COUNT(*) AS GameCount
+  if(USE_SQL){
+    val genreQuery = spark.sql(
+      s"""SELECT Genre, COUNT(*) AS GameCount
                   FROM (
                         SELECT TRIM(Genre) AS Genre
                         FROM steam_df
@@ -132,15 +208,26 @@ object SteamQueries extends App {
                   )
                   GROUP BY Genre
                   ORDER BY GameCount DESC""")
-  genreQuery.saveToCsv("GenreCount")
+
+    genreQuery.saveToCsv("SQL/GenreCount")
+  }
+  if(USE_COL_EXP){
+    val splittedGenresDf = convertedDF.withColumn("Genre", explode(split(col("Genres"), ",")))
+    val genreQuery = splittedGenresDf.withColumn("Genre", trim(col("Genre"))).groupBy("Genre").count()
+      .orderBy(desc("count"))
+
+    genreQuery.saveToCsv("NonSQL/GenreCount")
+  }
 
   //De esta última consulta, hemos obtenido que los géneros más comunes en Steam son:
   //"Indie", "Acción", "Casual", "Aventura", "Simulación", "RPG", "Estrategsuia" y "Deportes" en ese orden.
   //Sería interesante comparar el precio medio cada uno de estos géneros y  mediana
 
-    List("Avg", "Median").foreach(x =>
-    spark.sql(
-    s"""SELECT Genre, """ + (if (x eq "Avg") "AVG(PriceFloat)" else "percentile(PriceFloat, 0.5)") + s""" AS GenrePrice$x
+    List("Avg", "Median").foreach(x => {
+      if(USE_SQL){
+        spark.sql(
+          s"""SELECT Genre, """ + (if (x eq "Avg") "AVG(PriceFloat)" else "percentile(PriceFloat, 0.5)") +
+            s""" AS GenrePrice$x
                 FROM (
                       SELECT TRIM(Genre) AS Genre,
                             CAST(REGEXP_REPLACE(REGEXP_REPLACE(Price, '[$$€]', ''), ',', '.') AS float) AS PriceFloat
@@ -150,16 +237,37 @@ object SteamQueries extends App {
                 WHERE Genre IN ('Indie', 'Action', 'Casual', 'Adventure', 'Simulation', 'RPG', 'Strategy', 'Sports')
                 GROUP BY Genre
                 ORDER BY GenrePrice$x DESC"""
-    )
-    .saveToCsv(s"GenrePrice$x")
+        )
+          .saveToCsv(s"SQL/GenrePrice$x")
+      }
+      if(USE_COL_EXP){
+        val regexpPriceDf = convertedDF
+          .withColumn("PriceFloat", regexp_replace(col("Price"), "[$€]", ""))
+          .withColumn("PriceFloat", regexp_replace(col("PriceFloat"), ",", ".").cast("float"))
+
+        val trimmedGenreNPrice = regexpPriceDf
+          .withColumn("Genre", explode(split(col("Genres"), ",")))
+          .withColumn("Genre", trim(col("Genre")))
+          .select("Genre", "PriceFloat")
+
+        trimmedGenreNPrice.groupBy("Genre")
+          .agg({if (x eq "Avg") avg("PriceFloat") else expr("approx_percentile(PriceFloat, 0.5)")}.as(s"GenrePrice$x"))
+          .filter(col("Genre")
+            .isin("Indie", "Action", "Casual", "Adventure", "Simulation", "RPG", "Strategy", "Sports"))
+          .orderBy(desc(s"GenrePrice$x"))
+          .saveToCsv(s"NonSQL/GenrePrice$x")
+      }
+    }
   )
 
   //Parece que cuanto más popular es el género, menor precio suelen tener
   //Hagamos lo mismo con las categorías más comunes
 
-  List("Avg", "Median").foreach(x =>
-    spark.sql(
-      s"""SELECT Category, """ + (if (x eq "Avg") "AVG(PriceFloat)" else "percentile(PriceFloat, 0.5)") + s""" AS CategoryPrice$x
+  List("Avg", "Median").foreach(x => {
+    if(USE_SQL){
+      spark.sql(
+        s"""SELECT Category, """ + (if (x eq "Avg") "AVG(PriceFloat)" else "percentile(PriceFloat, 0.5)") +
+          s""" AS CategoryPrice$x
               FROM (
                     SELECT TRIM(Category) AS Category,
                           CAST(REGEXP_REPLACE(REGEXP_REPLACE(Price, '[$$€]', ''), ',', '.') AS float) AS PriceFloat
@@ -169,14 +277,34 @@ object SteamQueries extends App {
               WHERE Category IN ('Single-player', 'Steam Achievements', 'Downloadable Content', 'Steam Cloud', 'Full controller support', 'Multi-player')
               GROUP BY Category
               ORDER BY CategoryPrice$x DESC"""
-    )
-    .saveToCsv(s"CategoryPrice$x")
+      )
+        .saveToCsv(s"SQL/CategoryPrice$x")
+    }
+    if (USE_COL_EXP) {
+      val regexpPriceDf = convertedDF
+        .withColumn("PriceFloat", regexp_replace(col("Price"), "[$€]", ""))
+        .withColumn("PriceFloat", regexp_replace(col("PriceFloat"), ",", ".").cast("float"))
+
+      val trimmedCategoryNPrice = regexpPriceDf
+        .withColumn("Category", explode(split(col("Categories"), ",")))
+        .withColumn("Category", trim(col("Category")))
+        .select("Category", "PriceFloat")
+
+      trimmedCategoryNPrice.groupBy("Category")
+        .agg({if (x eq "Avg") avg("PriceFloat") else expr("approx_percentile(PriceFloat, 0.5)")}.as(s"CategoryPrice$x"))
+        .filter(col("Category")
+          .isin("Single-player", "Steam Achievements", "Downloadable Content", "Steam Cloud", "Full controller support", "Multi-player"))
+        .orderBy(desc(s"CategoryPrice$x"))
+        .saveToCsv(s"NonSQL/CategoryPrice$x")
+      }
+    }
   )
 
   //Por último, vamos a calcular el precio más común en cada género y caregoría
 
-  val mostFrequentGenrePriceQuery = spark.sql(
-    s"""WITH RankedValues AS (
+  if(USE_SQL){
+    val mostFrequentGenrePriceQuery = spark.sql(
+      s"""WITH RankedValues AS (
                       SELECT Genre, PriceFloat,
                             ROW_NUMBER() OVER (PARTITION BY Genre ORDER BY COUNT(PriceFloat) DESC) AS rank
                       FROM (
@@ -191,12 +319,36 @@ object SteamQueries extends App {
                 FROM RankedValues
                 WHERE rank = 1
                 ORDER BY PriceFloat DESC
-       """
-  )
-  mostFrequentGenrePriceQuery.saveToCsv("GenrePriceMostFrequent")
+       """)
 
-  val mostFrequentCategoryPriceQuery = spark.sql(
-    s"""WITH RankedValues AS (
+    mostFrequentGenrePriceQuery.saveToCsv("SQL/GenrePriceMostFrequent")
+  }
+  if(USE_COL_EXP){
+    val regexpPriceDf = convertedDF
+      .withColumn("PriceFloat", regexp_replace(col("Price"), "[$€]", ""))
+      .withColumn("PriceFloat", regexp_replace(col("PriceFloat"), ",", ".").cast("float"))
+
+    val trimmedGenreNPrice = regexpPriceDf
+      .withColumn("Genre", explode(split(col("Genres"), ",")))
+      .withColumn("Genre", trim(col("Genre")))
+      .select("Genre", "PriceFloat")
+
+    val partitions = Window.partitionBy("Genre").orderBy(desc("PriceCount"))
+
+    val mostFrequentGenrePriceQuery = trimmedGenreNPrice
+      .groupBy("Genre", "PriceFloat")
+      .agg(count("PriceFloat").as("PriceCount"))
+      .withColumn("rank", row_number().over(partitions))
+      .filter(col("rank") === 1)
+      .drop("rank", "PriceCount")
+      .orderBy(desc("PriceFloat"))
+
+    mostFrequentGenrePriceQuery.saveToCsv("NonSQL/GenrePriceMostFrequent")
+  }
+
+  if(USE_SQL){
+    val mostFrequentCategoryPriceQuery = spark.sql(
+      s"""WITH RankedValues AS (
                       SELECT Category, PriceFloat,
                             ROW_NUMBER() OVER (PARTITION BY Category ORDER BY COUNT(PriceFloat) DESC) AS rank
                       FROM (
@@ -212,8 +364,32 @@ object SteamQueries extends App {
                 WHERE rank = 1
                 ORDER BY PriceFloat DESC
        """
-  )
-  mostFrequentCategoryPriceQuery.saveToCsv("CategoryPriceMostFrequent")
+    )
+
+    mostFrequentCategoryPriceQuery.saveToCsv("SQL/CategoryPriceMostFrequent")
+  }
+  if (USE_COL_EXP) {
+    val regexpPriceDf = convertedDF
+      .withColumn("PriceFloat", regexp_replace(col("Price"), "[$€]", ""))
+      .withColumn("PriceFloat", regexp_replace(col("PriceFloat"), ",", ".").cast("float"))
+
+    val trimmedCategoryNPrice = regexpPriceDf
+      .withColumn("Category", explode(split(col("Categories"), ",")))
+      .withColumn("Category", trim(col("Category")))
+      .select("Category", "PriceFloat")
+
+    val partitions = Window.partitionBy("Category").orderBy(desc("PriceCount"))
+
+    val mostFrequentGenrePriceQuery = trimmedCategoryNPrice
+      .groupBy("Category", "PriceFloat")
+      .agg(count("PriceFloat").as("PriceCount"))
+      .withColumn("rank", row_number().over(partitions))
+      .filter(col("rank") === 1)
+      .drop("rank", "PriceCount")
+      .orderBy(desc("PriceFloat"))
+
+    mostFrequentGenrePriceQuery.saveToCsv("NonSQL/CategoryPriceMostFrequent")
+  }
 
 }
 
